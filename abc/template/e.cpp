e5153c4fde59f1dc8919a62603148f45fb43b38b
nekonekko <momoichigodayo+github@gmail.com>
2025-04-20 17:31:12 +0900

    feat: ✨ add iomanip include

--- abc/template/e.cpp
+++ abc/template/e.cpp
@@ -1,37 +1,36 @@
+#include <any>
 #include <cassert>
 #include <cfenv>
 #include <cfloat>
+#include <charconv>
+#include <cinttypes>
 #include <ciso646>
 #include <clocale>
+#include <codecvt>
+#include <condition_variable>
 #include <csetjmp>
 #include <csignal>
 #include <cstdbool>
-#include <cinttypes>
-#include <charconv>
-#include <typeindex>
-#include <any>
-#include <scoped_allocator>
+#include <ctgmath>
 #include <forward_list>
+#include <fstream>
+#include <future>
+#include <iomanip>
+#include <iostream>
 #include <list>
 #include <map>
+#include <queue>
+#include <random>
+#include <regex>
+#include <scoped_allocator>
 #include <set>
-#include <valarray>
-#include <variant>
+#include <shared_mutex>
+#include <typeindex>
 #include <unordered_map>
 #include <unordered_set>
-#include <queue>
-#include <condition_variable>
-#include <shared_mutex>
-#include <codecvt>
-#include <future>
-#include <regex>
-#include <iostream>
-#include <random>
-#include <ctgmath>
-#include <fstream>
+#include <valarray>
+#include <variant>
 
 using namespace std;
 
-int main() {
-    return 0;
-}
+int main() { return 0; }
